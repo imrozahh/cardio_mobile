b3c9a1bdc0c030654836be58c4aa1ef65a4e426b
Elyazid <elyazidmaulanaakbar@gmail.com>
2026-05-29 17:56:55 +0700

    update app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,3 +1,6 @@
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
+
 class AppConstants {
   AppConstants._();
 
@@ -5,8 +8,16 @@ class AppConstants {
   // Jika run di Android Emulator, gunakan 10.0.2.2
   // Jika run di Flutter Web / Chrome, gunakan 127.0.0.1 atau localhost
   // Jika run di HP fisik (satu WiFi), gunakan IP komputer Anda (misal: 192.168.x.x)
-  static const String baseUrl = 'http://127.0.0.1:8000/api';
-  static const String emulatorUrl = 'http://10.0.2.2:8000/api'; 
+  static String get baseUrl {
+    if (kIsWeb) {
+      return 'http://127.0.0.1:8000/api';
+    }
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8000/api';
+    }
+    return 'http://127.0.0.1:8000/api';
+  }
+  
   // static const String baseUrl = 'https://your-domain.com/api'; // Production
 
   static const int connectTimeout = 30000; // ms
